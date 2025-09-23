b24471e06c34f71d36d5105b0551ed5726b5a929
Rahul Pandita <rpandita1999@outlook.com>
2025-09-23 05:13:45 +0000

    add a new optional command-line argument for user prompt

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ from google import genai
 from google.genai import types
 
 def main():
-    # generate_response()
-    get_prompt_from_cmdl()
+    generate_response()
+    # get_prompt_from_cmdl()
 
 def get_api_key():
     load_dotenv()
@@ -15,16 +15,23 @@ def get_api_key():
 
 def generate_response():
     client = genai.Client(api_key=get_api_key())
+    user_prompt = get_prompt_from_cmdl()
+
     messages = [
-        types.Content(role="user", parts=[types.Part(text=get_prompt_from_cmdl())])
+        types.Content(role="user", parts=[types.Part(text=user_prompt.removesuffix("--verbose"))])
     ]
+
     response = client.models.generate_content(
         model="gemini-2.0-flash-001", 
         contents=messages
     )
     print(response.text)
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    if user_prompt.endswith("--verbose"):
+        print(f"User prompt: {user_prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    
+    
 
 def get_prompt_from_cmdl():
     cmd_args = sys.argv[1:]
