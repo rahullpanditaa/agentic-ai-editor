e47dcdac9b825c805b66e0acc56c8933879931b1
Rahul Pandita <rpandita1999@outlook.com>
2025-09-24 06:12:04 +0000

    FINALLY. Create function to get information about argument directory's contents; use pathlib instead of os.path

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,21 +1,32 @@
 import os
+from pathlib import Path
 import sys
 
-def get_files_info(boundary_directory, directory_to_check="."):
-    ...
-    # need to check whether the diretory_to_check is within the boundary directory
-    abs_boundary_dir_path = os.path.abspath(boundary_directory)
+def get_files_info(boundary_directory, directory="."):
+    # directory is relative path within the working directory
 
-    target_path = os.path.join(boundary_directory, directory_to_check)
-    
-    print(target_path)
-    print(abs_boundary_dir_path)
+    boundary_path = Path(boundary_directory)
+    boundary_path = boundary_path.resolve()
+    target_path = boundary_path / directory
 
-    print(os.path.abspath(target_path))
-    print(os.path.abspath(target_path).startswith(abs_boundary_dir_path))
+    # check whether target path inside working path
+    if not target_path.is_relative_to(boundary_path):
+        return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
     
+    if not target_path.is_dir():
+        return f'Error: "{directory}" is not a directory'
+    
+    directory_contents = ""
+    for p in target_path.iterdir():
+        directory_contents += f"- {p.name}: file_size={p.stat().st_size} bytes, is_dir={p.is_dir()}\n"
+    
+    return directory_contents.rstrip()
+
+print(get_files_info("calculator", "/bin"))
+    
+
     
     
 
-# get_files_info("calculator", directory_to_check="bin")
-get_files_info("calculator", directory_to_check=".")
+    
+    
