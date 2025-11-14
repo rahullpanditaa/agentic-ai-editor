bdfef5b1f41b29fd6e73499a007b76ca58711d59
Rahul Pandita <rpandita1999@outlook.com>
2025-11-14 07:32:17 +0000

    lots of refactoring; handle LLM returning a list of several FunctionCalls, not just one

--- main.py
+++ main.py
@@ -3,24 +3,40 @@ import sys
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
-from system_prompt import system_prompt
-from functions.function_schemas import schema_get_files_content, schema_get_files_info, schema_run_python_file, schema_write_file
+from .utils import (
+    MODEL,
+    SYSTEM_PROMPT, 
+    gemini_client_setup,
+)
+from functions.function_schemas import (
+    schema_get_files_content, 
+    schema_get_files_info, 
+    schema_run_python_file, 
+    schema_write_file
+)
 from call_function import call_function
 
 def main():
     generate_response()
 
-def get_api_key():
-    load_dotenv()
-    api_key = os.environ.get("GEMINI_API_KEY")
-    return api_key
+# def get_api_key():
+#     load_dotenv()
+#     api_key = os.environ.get("GEMINI_API_KEY")
+#     return api_key
 
 def generate_response():
-    client = genai.Client(api_key=get_api_key())
-    user_prompt = get_prompt_from_cmdl()
+    client = gemini_client_setup()
+    user_prompt = _get_prompt_from_cmdl()
+
+    # check if verbose flag is in command line args
     verbose = "--verbose" in sys.argv    
     
+    # Creating a Tool object/ instance of a Tool class
+    # tool - a function that LLM is allowed to call
+    # while generating a response
     available_functions = types.Tool(
+        # list of schemas i.e. function descriptions
+        # of the functions LLM can use
         function_declarations=[
             schema_get_files_info,
             schema_get_files_content,
@@ -28,42 +44,86 @@ def generate_response():
             schema_write_file
         ]
     )
+    # chat history
     messages = [
+        # Content object - contains the parts of a message
+        # role=user, marks as message coming from user in chat history
+        # parts = [..] ,list containing the content being sent in message
+        # initialize conversation history by sending a 'user' message, --verbose removed from end
         types.Content(role="user", parts=[types.Part(text=user_prompt.removesuffix("--verbose"))])
     ]
     try:
+        # Agent loop
         for _ in range(20):
+            # send a request to Gemini
             response = client.models.generate_content(
-                model="gemini-2.0-flash-001", 
+                model=MODEL, 
                 contents=messages,
-                config=types.GenerateContentConfig(tools=[available_functions], system_instruction=system_prompt)
+                config=types.GenerateContentConfig(tools=[available_functions], system_instruction=SYSTEM_PROMPT)
             )
+            # Gemini returns 1 or more candidates (i.e. possible outputs)
+            # loop through them all
             for candidate in response.candidates:
+                # if candidate contains content, and the content has some parts
                 if candidate.content and candidate.content.parts:
+                    # loop through each part, 
                     for part in candidate.content.parts:
+                        # if part contains text,
                         if part.text:
+                            # add the text to messages
                             messages.append(types.Content(role="model", parts=[types.Part(text=part.text)]))
-            #     messages.append(types.Content(role="model", parts=[types.Part(text=candidate.content)]))
             
+            # function_calls - list of function calls in response
+            # if this condition is True, model wants to execute 
+            # one of the available tools/functions
             if response.function_calls:
-                function_call_result = call_function(*response.function_calls, verbose=verbose)
-                messages.append(
-                types.Content(
-                    role="user", 
-                    parts=[types.Part(function_response=function_call_result.parts[0].function_response)]))
+                # a list of Contents, where each Content is 
+                # the result of calling the tool with its args
+                function_call_results = call_function(response.function_calls, verbose=verbose)
+                for function_call_result in function_call_results:
+                    messages.append(
+                        types.Content(
+                            role="model",
+                            parts=[types.Part(
+                                function_response=function_call_result.parts[0].function_response
+                            )]
+                        )
+                    )
+            
+            #     messages.append(
+            #     types.Content(
+            #         role="model", 
+            #         parts=[types.Part(function_response=function_call_result.parts[0].function_response)]))
+            
+            # LLM decides to produce text, not call function
             elif response.text:
                 print(response.text)
                 break
     except Exception as e:
         print(f"Error: {e}")    
 
-def get_prompt_from_cmdl():
+
+# grabs all args after main.py
+# if not provided, print error message, exit
+# convert into str, return
+def _get_prompt_from_cmdl():
     cmd_args = sys.argv[1:]
     if not cmd_args:
         print("Usage: uv run main.py \"write_prompt_here\"")
         sys.exit(1)
     prompt_entered = " ".join(cmd_args)
     return prompt_entered
+
+# def gemini_client_setup():
+#     load_dotenv()
+#     try:
+#         api_key = os.environ.get("GEMINI_API_KEY")
+#     except Exception as e:
+#         print("Unable to load GEMINI_API_KEY from .env file:")
+#         print(e)
+#     client = genai.Client(api_key=api_key)
+#     return client
+
 
 if __name__ == "__main__":
     main()
--- utils.py
+++ utils.py
@@ -1,4 +1,8 @@
-system_prompt = """
+import os
+from google import genai
+from dotenv import load_dotenv
+
+SYSTEM_PROMPT = """
     You are a helpful AI coding agent.
 
     When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
@@ -10,3 +14,15 @@ system_prompt = """
 
     All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
     """
+
+MODEL = "gemini-2.0-flash-001"
+
+def gemini_client_setup() -> genai.Client:
+    load_dotenv()
+    try:
+        api_key = os.environ.get("GEMINI_API_KEY")
+    except Exception as e:
+        print("Unable to load GEMINI_API_KEY from .env file:")
+        print(e)
+    client = genai.Client(api_key=api_key)
+    return client
